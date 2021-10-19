f80265d5ca6344397cd8ea6f282ed53f25f42b87
Tiffany Zambrano <92460321+Tiffanyzambrano@users.noreply.github.com>
2021-10-18 20:09:13 -0500

    EJEMPLO
    SE SUBIO UN ARCHIVO C´++

---
+++ 5 DIGITOS.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+ using namespace std;
+ int main(){
+ 	float n1,n2;
+ 	cout<<"INGRESE EL PRIMER NUMERO:";
+ 	cin>>n1;
+ 	cout<<"INGRESE EL SEGUNDO NUMERO:";
+ 	cin>>n2;
+ 	if (  n1>n2){
+	 cout<<"El  NUMERO  MAYOR ES:"<<n1;
+	 }
+	 else 
+	 if n>55555 y n<30000 {
+	 
+	 cout<<" TIENE 5 DIGITOS"
+ }
+ return 0;
+}
+ 
